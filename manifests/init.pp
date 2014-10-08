75893616e39cf609efa7d91dc06f0f75587d3b28
Adam Breaux <breauxaj@gmail.com>
2014-10-08 08:22:56 -0400

    standard preamble
    permit user selectable package state

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,38 @@
-class lighttpd {
+# Class: lighttpd
+#
+# This class installs the lighttpd package
+#
+# Parameters:
+#
+#  ensure: (default latest)
+#    Determine the state of the packages
+#
+# Actions:
+#   - Installs the lighttpd package
+#
+# Requires:
+#
+# Sample Usage:
+#
+#  For a standard installation, use:
+#
+#    class { 'lighttpd':
+#      ensure => 'latest'
+#    }
+#
+#  To remove the installation, use:
+#
+#    class { 'lighttpd':
+#      ensure => 'absent'
+#    }
+#
+class lighttpd (
+  $ensure = 'latest'
+){
   $required = $::operatingsystem ? {
     /(?i-mx:centos|fedora|redhat|scientific)/ => 'lighttpd',
   }
-    
-  package { $required: ensure => latest }
-  
+
+  package { $required: ensure => $ensure }
+
 }
